b30d82af7bbac93b79777977df30778272fb6f7d
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2025-04-30 15:16:37 +0600

    implemente grpc upload server

---
+++ readme.md
@@ -0,0 +1,4 @@
+# Video
+
+GET Url
+`http://127.0.0.1:8800/video/videosss.mp4`
--- src/run.rs
+++ src/run.rs
@@ -1,12 +1,22 @@
-use crate::rest::run::run_rest;
+use std::thread;
+
+use grpc_video_server::run_grpc_video_server;
+
+use crate::{rest::run::run_rest, utils::constraints::VIDEOS_SOURCE};
 
 pub fn run() {
-    // _ = thread::spawn(move || {
+    _ = thread::spawn(move || {
+        tokio::runtime::Runtime::new().unwrap().block_on(async {
+            match run_rest().await {
+                Ok(_) => println!("ok"),
+                Err(e) => println!("error: {:?}", e),
+            }
+        });
+    });
     tokio::runtime::Runtime::new().unwrap().block_on(async {
-        match run_rest().await {
+        match run_grpc_video_server(VIDEOS_SOURCE, "50051").await {
             Ok(_) => println!("ok"),
             Err(e) => println!("error: {:?}", e),
-        }
+        };
     });
-    // });
 }
